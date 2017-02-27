b6e7724dceb1eed954f160ffc7fc97f104ea6036
Alexander Matveev <blackkorol@gmail.com>
2017-02-27 19:48:12 +0300

    before auto updates

--- app/main.js
+++ app/main.js
@@ -62,27 +62,3 @@ function openDevTool(win, isOpen) {
     });
   }
 }
- 
-//-------------------------------------------------------------------
-//  MAC QUIT SECTION
-//-------------------------------------------------------------------
-// let template = []
-// if (process.platform === 'darwin') {
-//   // OS X
-//   const name = app.getName();
-//   template.unshift({
-//     label: name,
-//     submenu: [
-//       {
-//         label: 'About ' + name,
-//         role: 'about'
-//       },
-//       {
-//         label: 'Quit',
-//         accelerator: 'Command+Q',
-//         click() { app.quit(); }
-//       },
-//     ]
-//   })
-// }
-
