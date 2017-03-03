804fa8d020addd2a36587fbd8e1cad1aa4c4cbe0
Alexander Matveev <blackkorol@gmail.com>
2017-03-04 01:34:22 +0300

    crossplatform

--- app/main.js
+++ app/main.js
@@ -11,7 +11,7 @@ var config = require('./config/default');
 const devIsOpen = config.App.devTools;
 const os = require('os');
 
-// >>??
+
 // let template = []
 // if (process.platform === 'darwin') {
 //   // OS X
