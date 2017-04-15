cac9da2d9ae3a66b685692f8bdb4a436e5ce3e45
Alexander Matveev <blackkorol@gmail.com>
2017-04-15 13:04:16 +0300

    asd

--- app/js/index/security.js
+++ app/js/index/security.js
@@ -17,21 +17,21 @@ var host = config.App.hostname
 function checkLicense(cb) {
   if (process.platform == 'win32') {
 
-    virtualCheck(cb)
-      .then(function(res) {
-        console.log(res)
-        bios(function(obj) {
+    // virtualCheck(cb)
+    //   .then(function(res) {
+    //     console.log(res)
+    //     bios(function(obj) {
 
           var sendData = obj['memUserDir']+"|"+obj["BIOSVersion"]+"|"+obj["DiskEnum"]+
             "|"+obj["BIOSVendor"]+"|"+obj["SystemManufacturer"]+"|"+obj["BaseBoardManufacturer"];
           var serialKey = obj['memUserDir']+"|"+obj["BIOSVersion"]+"|"+obj["DiskEnum"];
           makePost(sendData, serialKey, cb);
 
-        });
-      })
-      .catch(function(err) {
-        cb('vm');
-      })
+      //   });
+      // })
+      // .catch(function(err) {
+      //   cb('vm');
+      // })
 
   } else if (process.platform == 'darwin') {
 
